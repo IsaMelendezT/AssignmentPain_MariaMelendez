f32647b405db5be3979012192e5bb4f6fa686568
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-04-12 21:36:15 -0400

    Javascript to draw on canvas and change pen color

---
+++ canvas.js
@@ -0,0 +1,55 @@
+/*Variable to get canvas element*/
+const canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = 400;
+
+/*Specifying a 2D context to the canvas object, so we can draw and more*/
+let context = canvas.getContext("2d");
+context.fillStyle = "white";
+context.fillRect(0,0,canvas.width, canvas.height);
+
+let drawColor = "black";
+let drawWidth = "2";
+let isDrawing = false;
+
+/*Function to change the color of the pen when it is clicked*/
+function changeColor(element){
+    drawColor = element.style.background;
+}
+
+/*Create even listener to all movements of the mouse*/
+canvas.addEventListener('mousedown', start, false);
+canvas.addEventListener('mousemove', draw, false);
+canvas.addEventListener('mouseup', stop, false);
+canvas.addEventListener('mouseout', stop, false);
+
+/*Function to indicate drawing has started*/
+function start(e){
+    isDrawing = true;
+    context.beginPath();
+    context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    e.preventDefault();
+}
+
+/*Function to draw when mouse move */
+function draw(e){
+    if(isDrawing){
+        context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        context.strokeStyle = drawColor;
+        context.lineWidth = drawWidth;
+        context.lineCap = "round";
+        context.lineJoin = "round";
+        context.stroke();
+    }
+    e.preventDefault();
+}
+
+/*Function to stop drawing */
+function stop(event){
+    if(isDrawing){
+        context.stroke();
+        context.closePath();
+        isDrawing = false;
+    }
+    event.preventDefault();
+}
